ca8595fcd8c57ea37e7767fe9ada8e824e11aea7
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-03-02 20:11:58 -0500

    reverser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	let word = '';
+	$: reverse = word.split('').reverse().join('');
 </script>
 
 <svelte:head>
@@ -16,6 +18,8 @@
 		>So I guess just hang out here and refresh the page every once in a while, I'm sure something
 		will show up eventually :)</span
 	>
+	<input style="text-align:center;" type="text" bind:value={word} placeholder="reverse some text" />
+	<p id="reversed-text">{reverse}</p>
 </section>
 
 <style>
@@ -33,5 +37,21 @@
 
 	span {
 		padding-block: 0.5rem;
+	}
+	input[type='text'] {
+		margin-top: 1rem;
+		border: none;
+		border-bottom: 1px solid var(--color-text);
+		background-color: transparent;
+		color: var(--color-theme-1);
+	}
+	input[type='text']:focus {
+		border: none;
+		border-bottom: 1px solid var(--color-text);
+		outline: none;
+	}
+	#reversed-text {
+		height: 1em;
+		color: var(--color-theme-2);
 	}
 </style>
