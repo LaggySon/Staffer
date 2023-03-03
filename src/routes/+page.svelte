d9a69cd9e7a1d6cbc27e677df9700fcbf97869d7
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-03-03 14:01:55 -0500

    nav box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,9 +20,14 @@
 	>
 	<input style="text-align:center;" type="text" bind:value={word} placeholder="reverse some text" />
 	<p id="reversed-text">{reverse}</p>
+
+	<div class="portal-box">
+		<a href="#" class="freelancer"><span>Freelancer Portal</span></a>
+		<a href="#" class="manager"><span>Manager Portal</span></a>
+	</div>
 </section>
 
-<style>
+<style lang="scss">
 	section {
 		display: flex;
 		flex-direction: column;
@@ -53,5 +58,80 @@
 	#reversed-text {
 		height: 1em;
 		color: var(--color-theme-2);
+	}
+
+	.portal-box {
+		outline: 2px solid black;
+		color: white;
+		font-weight: bold;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		height: 10rem;
+		text-align: center;
+		overflow: hidden;
+		font-size: 2rem;
+
+		& > * {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			color: white;
+			padding: 1rem;
+			transition: all 250ms;
+			position: relative;
+			&:hover {
+				text-decoration: none;
+			}
+		}
+
+		.freelancer {
+			background-color: var(--color-theme-1);
+
+			&::before {
+				transition: all 250ms;
+				width: 0%;
+				content: ' ';
+				position: absolute;
+				height: 100%;
+				z-index: 1;
+				background-color: var(--color-theme-1);
+				margin-left: 100%;
+			}
+			&:hover {
+				transition: all 250ms;
+				&::before {
+					content: ' ';
+					width: 100%;
+					margin-left: 200%;
+					// z-index: 0;
+					transition: all 250ms;
+				}
+			}
+		}
+		.manager {
+			background-color: var(--color-theme-2);
+
+			&::before {
+				transition: all 250ms;
+				width: 0%;
+				content: ' ';
+				position: absolute;
+				height: 100%;
+				z-index: 1;
+				background-color: var(--color-theme-2);
+				margin-right: 100%;
+			}
+			&:hover {
+				transition: all 250ms;
+				&::before {
+					content: ' ';
+					width: 100%;
+					margin-right: 200%;
+					// z-index: 0;
+					transition: all 250ms;
+				}
+			}
+		}
 	}
 </style>
