e9e215582521f8750c8dd52524135e1600afa393
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-04-03 18:44:27 -0400

    protected routes for org pages

--- src/routes/orgs/[org]/+page.server.ts
+++ src/routes/orgs/[org]/+page.server.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client';
+import { redirect } from '@sveltejs/kit';
 const prisma = new PrismaClient();
 
-export const load = async ({ params }: any) => {
+export const load = async ({ params, parent }: any) => {
+	const { session } = await parent();
 	const orgName = params.org;
 	const events = await prisma.event.findMany({
 		where: {
@@ -15,5 +17,21 @@ export const load = async ({ params }: any) => {
 			name: orgName
 		}
 	});
+	const validUsers = await prisma.organization.findUnique({
+		where: {
+			name: orgName
+		},
+		select: {
+			freelancers: {
+				select: { user: true }
+			}
+		}
+	});
+	const userList = validUsers?.freelancers.map((user: any) => {
+		return user.user.email;
+	});
+	if (!userList?.includes(session.user.email)) {
+		throw redirect(302, '/');
+	}
 	return { events, org };
 };
